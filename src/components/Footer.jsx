04a3774856438692b7fecf2007ba9cf2d16d798b
MicheleFumi <michele.fumi96@gmail.com>
2024-12-17 17:28:56 +0100

    fix className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ export default function Footer() {
                 <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
                     © 2024 Copyright:
                     <ul>
-                        <li><a class="text-body" href="https://github.com/Pietroo03">Pietro Ponte</a></li>
-                        <li><a class="text-body" href="https://github.com/Efestus31">Biagio Antonelli</a></li>
-                        <li><a class="text-body" href="https://github.com/MicheleFumi">Michele Fumi</a></li>
-                        <li><a class="text-body" href="https://github.com/giovanni-lumini">Giovanni Battista</a></li>
-                        <li><a class="text-body" href="https://github.com/nelvison-benedetto">Nelvison Benedetto</a></li>
+                        <li><a className="text-body" href="https://github.com/Pietroo03">Pietro Ponte</a></li>
+                        <li><a className="text-body" href="https://github.com/Efestus31">Biagio Antonelli</a></li>
+                        <li><a className="text-body" href="https://github.com/MicheleFumi">Michele Fumi</a></li>
+                        <li><a className="text-body" href="https://github.com/giovanni-lumini">Giovanni Battista</a></li>
+                        <li><a className="text-body" href="https://github.com/nelvison-benedetto">Nelvison Benedetto</a></li>
                     </ul>
                 </div>
                 {/* <!-- Copyright --> */}
