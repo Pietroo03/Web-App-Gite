750fa3331afad897efecf108fec58e06e84392e8
MicheleFumi <michele.fumi96@gmail.com>
2024-12-17 19:03:29 +0100

    change link to tripscard

--- src/components/TripsCard.jsx
+++ src/components/TripsCard.jsx
@@ -25,7 +25,7 @@ export default function TripsCard() {
                                             </div>
                                         </div>
                                         <div className='mt-3'>
-                                            <Link to={`trip/${trip.id}`} className='btn btn-primary fs-6'>Dettagli Viaggio</Link>
+                                            <Link to={`trip/${trip.destinazione}`} className='btn btn-primary fs-6'>Dettagli Viaggio</Link>
                                         </div>
                                     </div>
 
