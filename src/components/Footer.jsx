eda629b4140406849eb4da9560247b387c802799
Efestus31 <biagiohd@gmail.com>
2024-12-17 17:23:40 +0100

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+
+    return (
+        <>
+            <footer className="bg-body-tertiary text-center text-lg-start">
+                {/* <!-- Copyright --> */}
+                <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+                    © 2024 Copyright:
+                    <ul>
+                        <li><a class="text-body" href="https://github.com/Pietroo03">Pietro Ponte</a></li>
+                        <li><a class="text-body" href="https://github.com/Efestus31">Biagio Antonelli</a></li>
+                        <li><a class="text-body" href="https://github.com/MicheleFumi">Michele Fumi</a></li>
+                        <li><a class="text-body" href="https://github.com/giovanni-lumini">Giovanni Battista</a></li>
+                        <li><a class="text-body" href="https://github.com/nelvison-benedetto">Nelvison Benedetto</a></li>
+                    </ul>
+                </div>
+                {/* <!-- Copyright --> */}
+            </footer>
+        </>
+    )
+}
