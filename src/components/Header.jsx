b5caf1252d4b7098dff1863426a5c9e421d8d10a
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-17 17:52:40 +0100

    add style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,13 @@ export default function Header() {
 
     return (
         <>
-            <header>
+            <header className="d-flex justify-content-between bg-dark text-white p-4 shadow">
                 <h1>BoolTrips</h1>
-                <h2>Group 3</h2>
+                <nav className="d-flex align-items-center">
+                    <div className="p-2 fs-5">ciao</div>
+                    <div className="p-2 fs-5">ciao</div>
+                    <div className="p-2 fs-5">ciao</div>
+                </nav>
             </header>
         </>
     )
