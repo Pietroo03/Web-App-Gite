a79641325c9ee1c850e146967c137222da710fa6
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-17 15:00:00 +0100

    add routes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route element>
+
+            <Route index element />
+            <Route path="trip/:id" element />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
