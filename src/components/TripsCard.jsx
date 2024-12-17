766ea38c8abb7d3b1ae97a15ed877783b67525fb
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-17 18:21:55 +0100

    add style

--- src/components/TripsCard.jsx
+++ src/components/TripsCard.jsx
@@ -5,32 +5,42 @@ export default function TripsCard() {
 
     return (
         <>
-            {
-                trips.map(trip => (
-                    <div className="container" key={trip.id}>
-                        <h2 className="element-1">
-                            {trip.destinazione}
-                        </h2>
-                        <div>
-                            <div>Data D'inizio Viaggio:
-                                <span>{trip.dataInizio}</span>
+            <div className="container-sm">
+                <div className="row row-cols-1">
+                    {
+                        trips.map(trip => (
+                            <div className="col">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h2 className="element-1">
+                                            {trip.destinazione}
+                                        </h2>
+                                        <div>
+                                            <div>Data D'inizio Viaggio:
+                                                <span>{trip.dataInizio}</span>
+                                            </div>
+
+                                            <div>Data Di Fine Viaggio:
+                                                <span>{trip.dataFine}</span>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <Link to={`trip/${trip.id}`} className='btn btn-primary'>Dettagli Viaggio</Link>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
-
-                            <div>Data Di Fine Viaggio:
-                                <span>{trip.dataFine}</span>
-                            </div>
-                        </div>
-                        <div>
-                            <Link to={`trip/${trip.id}`}>Dettagli Viaggio</Link>
-                        </div>
-                    </div>
+                        ))
+                    }
+                </div>
+            </div>
 
 
 
 
 
-                ))
-            }
+
         </>
 
     )
