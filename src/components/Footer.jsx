e7e391e52570179879b2946e7f03a0e564353332
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-17 18:05:24 +0100

    add style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,16 @@ export default function Footer() {
 
     return (
         <>
-            <footer >
+            <footer className="bg-dark text-white p-4 d-flex justify-content-between">
                 <div>copyright</div>
+                <div className="d-flex flex-column">
+                    <a className="text-white link-underline link-underline-opacity-0" href="#">1</a>
+                    <a className="text-white link-underline link-underline-opacity-0" href="#">2</a>
+                    <a className="text-white link-underline link-underline-opacity-0" href="#">3</a>
+                    <a className="text-white link-underline link-underline-opacity-0" href="#">4</a>
+                    <a className="text-white link-underline link-underline-opacity-0" href="#">5</a>
+                    <a className="text-white link-underline link-underline-opacity-0" href="#">6</a>
+                </div>
             </footer>
         </>
     )
