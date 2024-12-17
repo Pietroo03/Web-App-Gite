14c23f8f3febbc3095a4ee81da8842fe1b426dd8
Efestus31 <biagiohd@gmail.com>
2024-12-17 17:05:21 +0100

    base layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+
+    return (
+        <>
+            <header>
+                <h1>BoolTrips</h1>
+                <h2>Group 3</h2>
+            </header>
+        </>
+    )
+}
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,8 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+
 export default function DefaultLayout() {
 
 
     return (
         <>
+            <Header />
+
+            <main>
+                <Outlet />
+            </main>
+
+            <Footer />
         </>
     )
 }
