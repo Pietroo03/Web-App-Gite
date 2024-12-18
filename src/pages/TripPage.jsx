577927a78ae7d20ed24b3a90a03ca9a5da98086e
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-18 16:24:20 +0100

    add icon searc add style to searchbar

--- src/pages/TripPage.jsx
+++ src/pages/TripPage.jsx
@@ -31,18 +31,26 @@ export default function TripPage() {
                     <div className="fs-5">{`${trip.dataInizio} / ${trip.dataFine}`}</div>
                 </div>
             }
+            <div className="d-flex justify-content-between align-items-middle">
 
-            <h1 className="">Partecipanti</h1>
+                <h1 className="">Partecipanti</h1>
 
-            {/* Barra di ricerca */}
-            <div className="mb-3">
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Cerca per nome o cognome"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}  // Aggiorna searchTerm al cambiamento
-                />
+                <div className="d-flex align-items-center">
+                    <i class="bi bi-search me-2 fs-5" style={{ transform: 'scaleX(-1)' }}></i>
+                    {/* Barra di ricerca */}
+                    <div className=" rounded border border-dark">
+
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Cerca per nome o cognome"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}  // Aggiorna searchTerm al cambiamento
+                        />
+                    </div>
+
+                </div>
+
             </div>
 
             <div className="row row-cols-1">
