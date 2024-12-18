bfdfe21bc157bb3e11935dfbad1b8b27a3f88afb
Pietroo03 <pietro.ponte.03@gmail.com>
2024-12-18 11:36:18 +0100

    add changes to style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
 
     return (
         <>
-            <header className="d-flex sticky-top justify-content-between align-items-center bg-dark text-white p-4 shadow">
+            <header className="d-flex justify-content-between align-items-center bg-dark text-white p-4 shadow">
                 <Link to="/" className="text-decoration-none text-white"><h1 className="mb-0">BoolTrips</h1></Link>
                 <nav className="d-flex align-items-center">
                     <div className="p-2 fs-5">ciao</div>
